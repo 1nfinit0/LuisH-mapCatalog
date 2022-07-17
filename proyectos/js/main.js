2094ac350a6f58beb8a40413907f937d4aa43e5f
1nfinit0 <noggnzzz@gmail.com>
2022-07-17 17:05:58 -0500

    Limites_nacionales

--- proyectos/js/main.js
+++ proyectos/js/main.js
@@ -1,33 +1,64 @@
 const files = [
-    {   href: '#',
-        src: '../assets/img/mapa_peru.png',
-        titulo:'Perú total - Límites nacionales',
-        descripcion:'Límite nacional del Perú, archivo vectorial. Contiene área, perímetro, población al 2022 en tabla atributiva. EPSG: 4326.',
+    {   href: './files/limiteNacional/limite_nacional.html',
+        src: './assets/img/limite_nacional_peru.png',
+        alt: 'No disponible',
+        titulo:'Perú total - Límite nacional',
+        descripcion:'Límite nacional del Perú, archivo vectorial. EPSG: 4326.',
         shp:'SHP',
         gml:'GML',
         bd:'PL/PgSQL',
-        geojeson:'GeoJson',
-        pdf:'PDF'
+        geojeson:'',
+        pdf:'PDF',
+        glb:''
+    },
+    {   href: '../target/model/modelo.html',
+        src: './assets/img/clegua_3d.png',
+        alt: 'No disponible',
+        titulo: 'Parque Media Luna - Carmen de La Legua | Modelo 3D',
+        descripcion:'Modelo 3D del parque Media Luna y alrededores del distrito de Carmen de La Legua -  Reynoso',
+        shp:'',
+        gml:'',
+        bd:'',
+        geojeson:'',
+        pdf:'',
+        glb:'GLB'
     },
 ]
 
 const formulario = document.querySelector('#formulario');
 const boton = document.querySelector('#boton');
-const proyecto = document.querySelector('#proyecto');
+const proyectos = document.querySelector('#proyectos');
+const acentos = {'á':'a','é':'e','í':'i','ó':'o','ú':'u','Á':'A','É':'E','Í':'I','Ó':'O','Ú':'U'};
 
  const filtrar = () => {
-    proyecto.innerHTML = '';
+    proyectos.innerHTML = '';
     const texto = formulario.value.toLowerCase();
+            texto.split('').map( letra => acentos[letra] || letra).join('').toString();
     for(let file of files){
         let titulo = file.titulo.toLowerCase();
+                    titulo = titulo.split('').map( letra => acentos[letra] || letra).join('').toString();
         if(titulo.indexOf(texto) !== -1){
-            proyecto.innerHTML += `
-            <a href="${files.href}"><img src="${files.src}"></img></a>
-            `
+            proyectos.innerHTML +="<div class='proyecto'><a href='"+file.href+"'><img src='"+file.src+"' alt='"+file.alt+"'></a>"+
+                                        "<div class='descripcion'>"+
+                                            "<a href='"+file.href+"'><h3>"+file.titulo+"</h3></a>"+
+                                            "<p>"+file.descripcion+"</p>"+
+                                            "<h4>Disponible en:</h4>"+
+                                                "<div class='formatos'>"+
+                                                    "<div class='shp'><p>"+file.shp+"</p></div>"+
+                                                    "<div class='gml'><p>"+file.gml+"</p></div>"+
+                                                    "<div class='bd'><p>"+file.bd+"</p></div>"+
+                                                    "<div class='geojson'><p>"+file.geojeson+"</p></div>"+
+                                                    "<div class='pdf'><p>"+file.pdf+"</p></div>"+
+                                                    "<div class='glb'><p>"+file.glb+"</p></div>"+
+                                                "</div>"+
+                                        "</div>"+
+                                    "</div><hr>";
+
+                                     ;
         }         
     }
-    if (proyecto.innerHTML === ''){
-        proyecto.innerHTML += `No se encontraron resultados`
+    if (proyectos.innerHTML === ''){
+        proyectos.innerHTML += `No se encontraron resultados`
     }
 
  }
